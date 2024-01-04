b7b61fd02c58a50cf903a930190bc85822f84369
Luka Signe--Morice <luka@signemorice.com>
2024-01-04 14:57:29 +0100

    Student tile style OK

--- client/src/components/StudentComponent.tsx
+++ client/src/components/StudentComponent.tsx
@@ -1,7 +1,16 @@
+import PlaceholderPic from '../assets/placeholder.png';
+import PlaceholderCompanyPic from '../assets/placeholder-company.png';
+
 export function StudentComponent() {
     return (
         <div className="student">
-            <p>Nom Prénom</p>
+            <img className="student-profile-picture" src={PlaceholderPic} alt="student"/>
+            <div className="student-overlay"></div>
+            <div className="student-main-infos">
+                <h3>Prénom</h3>
+                <h3>NOM</h3>
+                <img className="company-icon" src={PlaceholderCompanyPic} alt=""/>
+            </div>
         </div>
     )
 }
