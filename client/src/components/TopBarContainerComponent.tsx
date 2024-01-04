d76a27d0f4eaa7d2aa10e752520d81bfc8abc4c7
Luka Signe--Morice <luka@signemorice.com>
2024-01-04 23:54:25 +0100

    GraphQL query for program & promotion WIP

--- client/src/components/TopBarContainerComponent.tsx
+++ client/src/components/TopBarContainerComponent.tsx
@@ -1,17 +1,36 @@
 import React, { useState } from 'react';
 import { SelectorComponent } from "./SelectorComponent";
 import { SearchComponent } from "./SearchComponent";
+import {gql, request} from "graphql-request";
+import {GRAPHQL_ENDPOINT} from "../constants";
+import {useQuery} from "@tanstack/react-query";
 
 export function TopBarContainerComponent() {
-    const [selectedStudy, setSelectedStudy] = useState("FIL");
+    const [selectedProgram, setSelectedProgram] = useState("FIL");
     const [selectedPromotion, setSelectedPromotion] = useState("2024");
 
-    const studies = ["FIL", "FIT", "FISE"];
+    const programsQuery = gql`
+        query programs {
+            programs {
+                id,
+                name
+            }
+        }
+    `;
+
+    const loadPrograms = () => {request(GRAPHQL_ENDPOINT, programsQuery)};
+    const { isLoading, error, data, refetch } = useQuery({
+        queryKey: ['programs'],
+        queryFn: loadPrograms,
+    });
+    console.log(data)
+
+    const programs = ["FIL", "FIT", "FISE"];
     const promotions = ["2021", "2022", "2023", "2024", "2025", "2026", "2027"];
 
     return (
         <div id="top-bar">
-            <SelectorComponent selected={selectedStudy} options={studies} setSelected={setSelectedStudy} />
+            <SelectorComponent selected={selectedProgram} options={programs} setSelected={setSelectedProgram} />
             <SelectorComponent selected={selectedPromotion} options={promotions} setSelected={setSelectedPromotion} />
             <SearchComponent />
         </div>
