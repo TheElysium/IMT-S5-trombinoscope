97e1a14a94d940e36470e6635d01f3bacb2caf49
Luka Signe--Morice <luka@signemorice.com>
2024-01-05 16:58:57 +0100

    Centering tile on click - Need refacto

--- client/src/components/StudentComponent.tsx
+++ client/src/components/StudentComponent.tsx
@@ -1,16 +1,22 @@
 import PlaceholderPic from '../assets/placeholder.png';
 import PlaceholderCompanyPic from '../assets/placeholder-company.png';
-import {useRef} from "react";
+import {useEffect, useRef} from "react";
 
-export function StudentComponent({overlay}){
+export function StudentComponent({overlay, scrollPosition}){
     const studentTile = useRef(null);
     const studentDetailedInfos = useRef(null);
-    const handleClick = () => {
-        const translateX = overlay.current.offsetWidth/2 - studentTile.current.offsetLeft - studentTile.current.offsetWidth/2;
-        const translateY = overlay.current.offsetHeight/2 - studentTile.current.offsetTop - studentTile.current.offsetHeight/2;
-        studentTile.current.style.transform = `translate(${translateX}px, ${translateY}px)`;
 
+    const handleClick = () => {
+        studentDetailedInfos.current.style.display = "block";
+        studentDetailedInfos.current.style.opacity = "1";
+        studentDetailedInfos.current.style.left = `${studentTile.current.offsetWidth}px`;
+
+        console.log(scrollPosition)
+        const translateX = overlay.current.offsetWidth/2 - studentTile.current.offsetLeft - studentTile.current.offsetWidth/2 - studentDetailedInfos.current.offsetWidth/2;
+        const translateY = overlay.current.offsetHeight/2 - studentTile.current.offsetTop - studentTile.current.offsetHeight/2 + scrollPosition;
+        studentTile.current.style.transform = `translate(${translateX}px, ${translateY}px)`;
         studentTile.current.style.zIndex = "2";
+
         overlay.current.style.zIndex = "1";
         overlay.current.style.opacity = ".7";
         overlay.current.addEventListener("click", reset);
@@ -23,6 +29,10 @@ export function StudentComponent({overlay}){
         overlay.current.style.opacity = "0";
 
         overlay.current.removeEventListener("click", reset);
+
+        studentDetailedInfos.current.style.display = "none";
+        studentDetailedInfos.current.style.opacity = "0";
+        studentDetailedInfos.current.style.left = "0px";
 
         // Wait for the animation to end before resetting the z-index
         setTimeout(() => {
--- client/src/components/TrombinoscopeContainerComponent.tsx
+++ client/src/components/TrombinoscopeContainerComponent.tsx
@@ -1,37 +1,34 @@
 import PlusIcon from '../assets/plus.svg';
-import {StudentComponent} from "./StudentComponent";
-import {useRef} from "react";
+import { StudentComponent } from "./StudentComponent";
+import { useEffect, useRef, useState } from "react";
 
-export function TrombinoscopeContainerComponent(){
+export function TrombinoscopeContainerComponent() {
+    const [scrollPosition, setScrollPosition] = useState(0);
     const overlay = useRef(null);
+    const grid = useRef(null);
+
+    useEffect(() => {
+        overlay.current.style.top = `${scrollPosition}px`;
+    }, [scrollPosition]);
+
+    const updateOverlayPosition = () => {
+        setScrollPosition(grid.current.scrollTop);
+    };
 
     const students = () => {
         const students = [];
         for (let i = 0; i < 30; i++) {
-            students.push(<StudentComponent overlay={overlay}/>)
+            students.push(<StudentComponent overlay={overlay} scrollPosition={scrollPosition}/>);
         }
         return students;
-    }
+    };
+
     return (
-/*        <div id="trombinoscope-container">
-            <div id="carousel-arrow-up">
-                <img src={ArrowIcon} alt="Arrow icon up"/>
-            </div>
-            <div id="trombinoscope-grid-container">
-                <div id="trombinoscope-grid">
-                    {students()}
-                </div>
-            </div>
-            <div id="carousel-arrow-down">
-                <img src={ArrowIcon} alt="Arrow icon down"/>
-            </div>
-            <div id="carousel-selector"></div>
-        </div>*/
-        <div id="trombinoscope-grid-container">
+        <div id="trombinoscope-grid-container" onScroll={updateOverlayPosition} ref={grid}>
             <div id="trombinoscope-grid">
                 <div id="new-student">
                     <div id="new-student-content">
-                        <img src={PlusIcon} alt=""/>
+                        <img src={PlusIcon} alt="" />
                         ajouter
                     </div>
                 </div>
@@ -39,5 +36,5 @@ export function TrombinoscopeContainerComponent(){
             </div>
             <div id="trombinoscope-grid-overlay" ref={overlay}></div>
         </div>
-        )
+    );
 }
