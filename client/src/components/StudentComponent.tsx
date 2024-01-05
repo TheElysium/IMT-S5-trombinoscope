33ab915520cfdc37602ce3491e537165b1e25f69
Luka Signe--Morice <luka@signemorice.com>
2024-01-05 14:31:55 +0100

    Centering tile on click OK

--- client/src/components/StudentComponent.tsx
+++ client/src/components/StudentComponent.tsx
@@ -3,16 +3,34 @@ import PlaceholderCompanyPic from '../assets/placeholder-company.png';
 import {useRef} from "react";
 
 export function StudentComponent({overlay}){
-    const ref = useRef(null);
+    const studentTile = useRef(null);
+    const studentDetailedInfos = useRef(null);
     const handleClick = () => {
-        ref.current.style.transform = `translateX(${overlay.current.offsetWidth/2 - ref.current.offsetLeft - ref.current.offsetWidth/2}px)`;
-        ref.current.style.transform += `translateY(${overlay.current.offsetHeight/2 - ref.current.offsetTop - ref.current.offsetHeight/2}px)`;
-        ref.current.style.zIndex = "2";
+        const translateX = overlay.current.offsetWidth/2 - studentTile.current.offsetLeft - studentTile.current.offsetWidth/2;
+        const translateY = overlay.current.offsetHeight/2 - studentTile.current.offsetTop - studentTile.current.offsetHeight/2;
+        studentTile.current.style.transform = `translate(${translateX}px, ${translateY}px)`;
+
+        studentTile.current.style.zIndex = "2";
         overlay.current.style.zIndex = "1";
         overlay.current.style.opacity = ".7";
+        overlay.current.addEventListener("click", reset);
+    }
+
+    const reset = () => {
+        studentTile.current.style.transform = `translate(0px, 0px)`;
+
+        overlay.current.style.zIndex = "-1";
+        overlay.current.style.opacity = "0";
+
+        overlay.current.removeEventListener("click", reset);
+
+        // Wait for the animation to end before resetting the z-index
+        setTimeout(() => {
+            studentTile.current.style.zIndex = "0";
+        }, 300);
     }
     return (
-        <div className="student" ref={ref} onClick={handleClick}>
+        <div className="student" ref={studentTile} onClick={handleClick}>
             <img className="student-profile-picture" src={PlaceholderPic} alt="student"/>
             <div className="student-overlay"></div>
             <div className="student-main-infos">
@@ -20,7 +38,7 @@ export function StudentComponent({overlay}){
                 <h3>NOM</h3>
                 <img className="company-icon" src={PlaceholderCompanyPic} alt=""/>
             </div>
-            <div className="student-detailed-infos">
+            <div className="student-detailed-infos" ref={studentDetailedInfos}>
                 <p><span>Promotion :</span> FIL 2024</p>
                 <p><span>Promotion :</span> FIL 2024</p>
                 <p><span>Promotion :</span> FIL 2024</p>
